e8f49898f1df4663cd8566b2c9327a90af660c50
Maryline <maryline.demichelis@gmail.com>
2016-01-07 12:54:21 +0100

    initial commit

---
+++ PolySims/src/code_source/Main.java
@@ -0,0 +1,15 @@
+package code_source;
+
+public class Main {
+
+	public static void main(String[] args){
+		Polyprof Tisserant = new Polyprof() ;
+		Tisserant.corrigerCopies();
+		Tisserant.corrigerCopies();
+		Tisserant.corrigerCopies();
+		Tisserant.allerToilette() ;
+		Tisserant.dormir();
+		Tisserant.seDeplacer("soirée");
+		
+	}
+}
